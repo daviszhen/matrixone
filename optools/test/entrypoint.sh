455d94f07881d1c9b64fae6e0ee5d4df063135d0
daviszhen <daviszhen007@gmail.com>
2022-04-11 20:42:13 +0800

    update

--- optools/test/entrypoint.sh
+++ optools/test/entrypoint.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 sed -i "s/HOST/${HOST}/g" system_vars_config.toml
 sed -i "s/NAME/${NAME}/g" system_vars_config.toml
 
-./mo-server ./system_vars_config.toml
+/usr/local/bin/dumb-init ./mo-server ./system_vars_config.toml
