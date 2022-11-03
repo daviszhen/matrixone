bcc9f9ce58a785f6e9d4f5e6664e532d6339fcfc
daviszhen <daviszhen007@gmail.com>
2022-11-03 21:08:42 +0800

    fix ut

--- pkg/frontend/mysql_cmd_executor_test.go
+++ pkg/frontend/mysql_cmd_executor_test.go
@@ -56,8 +56,8 @@ func init() {
 func mockRecordStatement(ctx context.Context) (context.Context, *gostub.Stubs) {
 	stm := &trace.StatementInfo{}
 	ctx = trace.ContextWithStatement(ctx, stm)
-	stubs := gostub.Stub(&RecordStatement, func(context.Context, *Session, *process.Process, ComputationWrapper, time.Time, string, bool) context.Context {
-		return ctx
+	stubs := gostub.Stub(&RecordStatement, func(context.Context, *Session, *process.Process, ComputationWrapper, time.Time, string, bool) (context.Context, error) {
+		return ctx, nil
 	})
 	return ctx, stubs
 }
