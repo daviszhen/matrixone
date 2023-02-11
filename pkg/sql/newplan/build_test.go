64651876b81894bc279b0037d8942b0ba1f66081
daviszhen <daviszhen007@gmail.com>
2023-02-11 17:29:29 +0800

    add q9

--- pkg/sql/newplan/build_test.go
+++ pkg/sql/newplan/build_test.go
@@ -507,6 +507,48 @@ func Test_build16_q8(t *testing.T) {
 	})
 }
 
+func Test_build16_q9(t *testing.T) {
+	convey.Convey("tpch-q9", t, func() {
+		sql := `select
+					nation,
+					o_year,
+					sum(amount) as sum_profit
+				from
+					(
+						select
+							n_name as nation,
+							extract(year from o_orderdate) as o_year,
+							l_extendedprice * (1 - l_discount) - ps_supplycost * l_quantity as amount
+						from
+							part,
+							supplier,
+							lineitem,
+							partsupp,
+							orders,
+							nation
+						where
+							s_suppkey = l_suppkey
+							and ps_suppkey = l_suppkey
+							and ps_partkey = l_partkey
+							and p_partkey = l_partkey
+							and o_orderkey = l_orderkey
+							and s_nationkey = n_nationkey
+							and p_name like '%pink%'
+					) as profit
+				group by
+					nation,
+					o_year
+				order by
+					nation,
+					o_year desc
+				;
+				`
+		ret, err := runCase(sql)
+		convey.So(err, convey.ShouldBeNil)
+		convey.So(ret, convey.ShouldBeTrue)
+	})
+}
+
 func Test_Debug(t *testing.T) {
 	cc := sqlplan.NewMockCompilerContext()
 	sql := `select
